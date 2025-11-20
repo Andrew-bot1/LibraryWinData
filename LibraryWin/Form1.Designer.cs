25f650fdbc6c3580673cb8732c6079649893c62b
Andrew <Andrew@BB21312>
2025-11-20 16:23:30 -0600

    Front-end done?

--- LibraryWin/Form1.Designer.cs
+++ LibraryWin/Form1.Designer.cs
@@ -46,12 +46,25 @@
             this.txtGenre = new System.Windows.Forms.TextBox();
             this.txtQuan = new System.Windows.Forms.TextBox();
             this.txtPages = new System.Windows.Forms.TextBox();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.lblLength = new System.Windows.Forms.Label();
+            this.txtNar = new System.Windows.Forms.TextBox();
+            this.lblNar = new System.Windows.Forms.Label();
+            this.txtDis = new System.Windows.Forms.TextBox();
+            this.lblDis = new System.Windows.Forms.Label();
+            this.txtIllPgs = new System.Windows.Forms.TextBox();
+            this.lblIllPgs = new System.Windows.Forms.Label();
+            this.txtIll = new System.Windows.Forms.TextBox();
+            this.lblIll = new System.Windows.Forms.Label();
+            this.list = new System.Windows.Forms.ListBox();
+            this.btnDis = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblName
             // 
             this.lblName.AutoSize = true;
-            this.lblName.Location = new System.Drawing.Point(283, 71);
+            this.lblName.Location = new System.Drawing.Point(269, 29);
             this.lblName.Name = "lblName";
             this.lblName.Size = new System.Drawing.Size(35, 13);
             this.lblName.TabIndex = 0;
@@ -60,7 +73,7 @@
             // lblISBN
             // 
             this.lblISBN.AutoSize = true;
-            this.lblISBN.Location = new System.Drawing.Point(283, 108);
+            this.lblISBN.Location = new System.Drawing.Point(269, 66);
             this.lblISBN.Name = "lblISBN";
             this.lblISBN.Size = new System.Drawing.Size(32, 13);
             this.lblISBN.TabIndex = 1;
@@ -69,7 +82,7 @@
             // lblAuthor
             // 
             this.lblAuthor.AutoSize = true;
-            this.lblAuthor.Location = new System.Drawing.Point(283, 148);
+            this.lblAuthor.Location = new System.Drawing.Point(269, 106);
             this.lblAuthor.Name = "lblAuthor";
             this.lblAuthor.Size = new System.Drawing.Size(38, 13);
             this.lblAuthor.TabIndex = 2;
@@ -78,7 +91,7 @@
             // lblPrice
             // 
             this.lblPrice.AutoSize = true;
-            this.lblPrice.Location = new System.Drawing.Point(283, 192);
+            this.lblPrice.Location = new System.Drawing.Point(266, 146);
             this.lblPrice.Name = "lblPrice";
             this.lblPrice.Size = new System.Drawing.Size(31, 13);
             this.lblPrice.TabIndex = 3;
@@ -87,7 +100,7 @@
             // lblGenre
             // 
             this.lblGenre.AutoSize = true;
-            this.lblGenre.Location = new System.Drawing.Point(283, 235);
+            this.lblGenre.Location = new System.Drawing.Point(269, 193);
             this.lblGenre.Name = "lblGenre";
             this.lblGenre.Size = new System.Drawing.Size(36, 13);
             this.lblGenre.TabIndex = 4;
@@ -96,7 +109,7 @@
             // lblQuan
             // 
             this.lblQuan.AutoSize = true;
-            this.lblQuan.Location = new System.Drawing.Point(283, 272);
+            this.lblQuan.Location = new System.Drawing.Point(269, 230);
             this.lblQuan.Name = "lblQuan";
             this.lblQuan.Size = new System.Drawing.Size(46, 13);
             this.lblQuan.TabIndex = 5;
@@ -105,11 +118,12 @@
             // lblPages
             // 
             this.lblPages.AutoSize = true;
-            this.lblPages.Location = new System.Drawing.Point(283, 319);
+            this.lblPages.Location = new System.Drawing.Point(269, 277);
             this.lblPages.Name = "lblPages";
             this.lblPages.Size = new System.Drawing.Size(37, 13);
             this.lblPages.TabIndex = 6;
             this.lblPages.Text = "Pages";
+            this.lblPages.Visible = false;
             // 
             // radBook
             // 
@@ -121,6 +135,7 @@
             this.radBook.TabStop = true;
             this.radBook.Text = "Book";
             this.radBook.UseVisualStyleBackColor = true;
+            this.radBook.CheckedChanged += new System.EventHandler(this.radBook_CheckedChanged);
             // 
             // radAud
             // 
@@ -132,6 +147,7 @@
             this.radAud.TabStop = true;
             this.radAud.Text = "Audio Book";
             this.radAud.UseVisualStyleBackColor = true;
+            this.radAud.CheckedChanged += new System.EventHandler(this.radAud_CheckedChanged);
             // 
             // radDis
             // 
@@ -143,6 +159,7 @@
             this.radDis.TabStop = true;
             this.radDis.Text = "Discount Book";
             this.radDis.UseVisualStyleBackColor = true;
+            this.radDis.CheckedChanged += new System.EventHandler(this.radDis_CheckedChanged);
             // 
             // radIll
             // 
@@ -154,61 +171,192 @@
             this.radIll.TabStop = true;
             this.radIll.Text = "Illustrated Book";
             this.radIll.UseVisualStyleBackColor = true;
+            this.radIll.CheckedChanged += new System.EventHandler(this.radIll_CheckedChanged);
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(163, 71);
+            this.txtName.Location = new System.Drawing.Point(149, 29);
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(100, 20);
             this.txtName.TabIndex = 11;
             // 
             // txtISBN
             // 
-            this.txtISBN.Location = new System.Drawing.Point(163, 105);
+            this.txtISBN.Location = new System.Drawing.Point(149, 63);
             this.txtISBN.Name = "txtISBN";
             this.txtISBN.Size = new System.Drawing.Size(100, 20);
             this.txtISBN.TabIndex = 12;
             // 
             // txtAuthor
             // 
-            this.txtAuthor.Location = new System.Drawing.Point(163, 145);
+            this.txtAuthor.Location = new System.Drawing.Point(149, 103);
             this.txtAuthor.Name = "txtAuthor";
             this.txtAuthor.Size = new System.Drawing.Size(100, 20);
             this.txtAuthor.TabIndex = 13;
             // 
             // txtPrice
             // 
-            this.txtPrice.Location = new System.Drawing.Point(163, 192);
+            this.txtPrice.Location = new System.Drawing.Point(146, 146);
             this.txtPrice.Name = "txtPrice";
             this.txtPrice.Size = new System.Drawing.Size(100, 20);
             this.txtPrice.TabIndex = 14;
             // 
             // txtGenre
             // 
-            this.txtGenre.Location = new System.Drawing.Point(163, 235);
+            this.txtGenre.Location = new System.Drawing.Point(149, 193);
             this.txtGenre.Name = "txtGenre";
             this.txtGenre.Size = new System.Drawing.Size(100, 20);
             this.txtGenre.TabIndex = 15;
             // 
             // txtQuan
             // 
-            this.txtQuan.Location = new System.Drawing.Point(163, 272);
+            this.txtQuan.Location = new System.Drawing.Point(149, 230);
             this.txtQuan.Name = "txtQuan";
             this.txtQuan.Size = new System.Drawing.Size(100, 20);
             this.txtQuan.TabIndex = 16;
             // 
             // txtPages
             // 
-            this.txtPages.Location = new System.Drawing.Point(163, 319);
+            this.txtPages.Location = new System.Drawing.Point(149, 277);
             this.txtPages.Name = "txtPages";
             this.txtPages.Size = new System.Drawing.Size(100, 20);
             this.txtPages.TabIndex = 17;
+            this.txtPages.Visible = false;
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(149, 318);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(100, 20);
+            this.txtLength.TabIndex = 19;
+            this.txtLength.Visible = false;
+            // 
+            // lblLength
+            // 
+            this.lblLength.AutoSize = true;
+            this.lblLength.Location = new System.Drawing.Point(269, 318);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(40, 13);
+            this.lblLength.TabIndex = 18;
+            this.lblLength.Text = "Length";
+            this.lblLength.Visible = false;
+            // 
+            // txtNar
+            // 
+            this.txtNar.Location = new System.Drawing.Point(149, 277);
+            this.txtNar.Name = "txtNar";
+            this.txtNar.Size = new System.Drawing.Size(100, 20);
+            this.txtNar.TabIndex = 21;
+            this.txtNar.Visible = false;
+            // 
+            // lblNar
+            // 
+            this.lblNar.AutoSize = true;
+            this.lblNar.Location = new System.Drawing.Point(269, 277);
+            this.lblNar.Name = "lblNar";
+            this.lblNar.Size = new System.Drawing.Size(45, 13);
+            this.lblNar.TabIndex = 20;
+            this.lblNar.Text = "Narrator";
+            this.lblNar.Visible = false;
+            // 
+            // txtDis
+            // 
+            this.txtDis.Location = new System.Drawing.Point(149, 277);
+            this.txtDis.Name = "txtDis";
+            this.txtDis.Size = new System.Drawing.Size(100, 20);
+            this.txtDis.TabIndex = 23;
+            this.txtDis.Visible = false;
+            // 
+            // lblDis
+            // 
+            this.lblDis.AutoSize = true;
+            this.lblDis.Location = new System.Drawing.Point(269, 277);
+            this.lblDis.Name = "lblDis";
+            this.lblDis.Size = new System.Drawing.Size(49, 13);
+            this.lblDis.TabIndex = 22;
+            this.lblDis.Text = "Discount";
+            this.lblDis.Visible = false;
+            // 
+            // txtIllPgs
+            // 
+            this.txtIllPgs.Location = new System.Drawing.Point(149, 366);
+            this.txtIllPgs.Name = "txtIllPgs";
+            this.txtIllPgs.Size = new System.Drawing.Size(100, 20);
+            this.txtIllPgs.TabIndex = 27;
+            this.txtIllPgs.Visible = false;
+            // 
+            // lblIllPgs
+            // 
+            this.lblIllPgs.AutoSize = true;
+            this.lblIllPgs.Location = new System.Drawing.Point(269, 369);
+            this.lblIllPgs.Name = "lblIllPgs";
+            this.lblIllPgs.Size = new System.Drawing.Size(85, 13);
+            this.lblIllPgs.TabIndex = 26;
+            this.lblIllPgs.Text = "Illustrated Pages";
+            this.lblIllPgs.Visible = false;
+            // 
+            // txtIll
+            // 
+            this.txtIll.Location = new System.Drawing.Point(149, 318);
+            this.txtIll.Name = "txtIll";
+            this.txtIll.Size = new System.Drawing.Size(100, 20);
+            this.txtIll.TabIndex = 25;
+            this.txtIll.Visible = false;
+            // 
+            // lblIll
+            // 
+            this.lblIll.AutoSize = true;
+            this.lblIll.Location = new System.Drawing.Point(266, 318);
+            this.lblIll.Name = "lblIll";
+            this.lblIll.Size = new System.Drawing.Size(49, 13);
+            this.lblIll.TabIndex = 24;
+            this.lblIll.Text = "Illustrator";
+            this.lblIll.Visible = false;
+            // 
+            // list
+            // 
+            this.list.FormattingEnabled = true;
+            this.list.Location = new System.Drawing.Point(450, 29);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(233, 160);
+            this.list.TabIndex = 28;
+            // 
+            // btnDis
+            // 
+            this.btnDis.Location = new System.Drawing.Point(536, 212);
+            this.btnDis.Name = "btnDis";
+            this.btnDis.Size = new System.Drawing.Size(75, 23);
+            this.btnDis.TabIndex = 29;
+            this.btnDis.Text = "Display";
+            this.btnDis.UseVisualStyleBackColor = true;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(17, 267);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 30;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.btnDis);
+            this.Controls.Add(this.list);
+            this.Controls.Add(this.txtIllPgs);
+            this.Controls.Add(this.lblIllPgs);
+            this.Controls.Add(this.txtIll);
+            this.Controls.Add(this.lblIll);
+            this.Controls.Add(this.txtDis);
+            this.Controls.Add(this.lblDis);
+            this.Controls.Add(this.txtNar);
+            this.Controls.Add(this.lblNar);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.lblLength);
             this.Controls.Add(this.txtPages);
             this.Controls.Add(this.txtQuan);
             this.Controls.Add(this.txtGenre);
@@ -254,6 +402,19 @@
         private System.Windows.Forms.TextBox txtGenre;
         private System.Windows.Forms.TextBox txtQuan;
         private System.Windows.Forms.TextBox txtPages;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.Label lblLength;
+        private System.Windows.Forms.TextBox txtNar;
+        private System.Windows.Forms.Label lblNar;
+        private System.Windows.Forms.TextBox txtDis;
+        private System.Windows.Forms.Label lblDis;
+        private System.Windows.Forms.TextBox txtIllPgs;
+        private System.Windows.Forms.Label lblIllPgs;
+        private System.Windows.Forms.TextBox txtIll;
+        private System.Windows.Forms.Label lblIll;
+        private System.Windows.Forms.ListBox list;
+        private System.Windows.Forms.Button btnDis;
+        private System.Windows.Forms.Button btnCreate;
     }
 }
 
--- LibraryWin/Form1.cs
+++ LibraryWin/Form1.cs
@@ -16,5 +16,55 @@ namespace LibraryWin
         {
             InitializeComponent();
         }
+
+        private void radAud_CheckedChanged(object sender, EventArgs e)
+        {
+            HideAll();
+            lblLength.Visible = true;
+            txtLength.Visible = true;
+            lblNar.Visible = true;
+            txtNar.Visible = true;
+        }
+
+        private void radBook_CheckedChanged(object sender, EventArgs e)
+        {
+            HideAll();
+            lblPages.Visible = true;
+            txtPages.Visible = true;
+        }
+
+        private void HideAll()
+        {
+            lblLength.Visible = false;
+            txtLength.Visible = false;
+            lblNar.Visible = false;
+            txtNar.Visible = false;
+            lblPages.Visible = false;
+            txtPages.Visible = false;
+            lblDis.Visible = false;
+            txtDis.Visible = false;
+            lblIll.Visible = false;
+            txtIll.Visible = false;
+            lblIllPgs.Visible = false;
+            txtIllPgs.Visible = false;
+        }
+
+        private void radDis_CheckedChanged(object sender, EventArgs e)
+        {
+            HideAll();
+            lblDis.Visible = true;
+            txtDis.Visible = true;
+        }
+
+        private void radIll_CheckedChanged(object sender, EventArgs e)
+        {
+            HideAll();
+            lblIll.Visible = true;
+            txtIll.Visible = true;
+            lblIllPgs.Visible = true;
+            txtIllPgs.Visible = true;
+            lblPages.Visible = true;
+            txtPages.Visible = true;
+        }
     }
 }
